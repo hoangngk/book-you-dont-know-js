28dc2501e0f560a0fc5621168bf79c902f7fa9e8
Hoang Nguyen Khanh <n.khanhhoang@gmail.com>
2017-06-15 15:12:06 +0700

    first commit

---
+++ modules/ie1.js
@@ -0,0 +1,30 @@
+// The most common usage of closure in JS is the module pattern
+
+function User() {
+  // usr, password is private
+  var username, password;
+  var publicAPI = {};
+
+  // private also. You can't access outside function
+  function doLogin(user, pw) {
+    username = user;
+    password = pw;
+    // do the rest of the login work
+  }
+
+  publicAPI = {
+    login: doLogin
+  };
+
+  return publicAPI;
+}
+
+
+// We are not calling new User() here, on purpose, despite the fact that probably seems more common
+// to most readers. User() is just a function, not a class to be instantiated, so it’s just called
+// normally. Using new would be inappropriate and actually waste resources.
+var fred = User();
+
+fred.login('fred', '12345');
+
+
---
+++ this.js
@@ -0,0 +1,25 @@
+
+//If a function has a this reference inside it,
+// that this reference usually points to an object.
+// But which object it points to depends on how the function was called.
+function foo() {
+  console.log(this.bar);
+}
+
+var bar = 'global';
+
+var obj1 = {
+  bar: 'obj1',
+  foo: foo
+}
+
+var obj2 = {
+  bar: 'obj2'
+}
+
+foo(); // globals
+
+obj1.foo(); // obj1
+
+foo.call(obj2);
+
